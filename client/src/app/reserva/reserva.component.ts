50bb4f783d5f4a60d86a279685411d7758f86fe9
Francesc <elskater98@gmail.com>
2020-04-18 18:07:24 +0200

    Table Done

--- client/src/app/reserva/reserva.component.ts
+++ client/src/app/reserva/reserva.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef, MatSnackBar} from '@angular/material';
+import {MatDialog, MatDialogRef, MatSnackBar, MatTableDataSource} from '@angular/material';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../services/authentication.service';
 import {ReservaCreateComponent} from './reserva-create/reserva-create.component';
@@ -18,6 +18,9 @@ export class ReservaComponent implements OnInit {
   public date:Date;
   public isDay:boolean;
   public inside:boolean;
+  public displayedColumns:string[]=['position','client','people','hour','observations'];
+  public dataSource: MatTableDataSource<any>;
+  public totalPeople:number;
 
   constructor(
     private router: Router,
@@ -34,6 +37,7 @@ export class ReservaComponent implements OnInit {
     this.isDay=true;
     this.inside=true;
     this.getReservasInsite();
+    this.totalPeople=0;
   }
 
   newReserva(){
@@ -47,8 +51,26 @@ export class ReservaComponent implements OnInit {
     let aux = this.isDay ? " Lunch":" Diner";
     let subId= this.generateSubId(this.date);
 
-    this.reservaService.findBySubIdAndInside(subId+aux).subscribe(data=>{
-      this.listReservas=data;
+    this.reservaService.findBySubIdAndInside(subId+aux).subscribe((data:any) =>{
+      let aux_list=[];
+      for(let i=0; i<data.length;i++){
+        aux_list.push({
+          'position':i+1,
+          'id': data[i]['id'],
+          'client': data[i]['client'],
+          'people': data[i]['people'],
+          'subId': data[i]['subId'],
+          'date': data[i]['date'],
+          'dateString':this.datePipe.transform(data[i]['dateString'], 'dd-MM-yyyy'),
+          'hour': data[i]['hour'],
+          'inside':data[i]['inside'],
+          'mobile':data[i]['mobile'],
+          'email':data[i]['email'],
+          'observations':data[i]['observations']
+        });
+      }
+      this.listReservas=aux_list;
+      this.dataSource = new MatTableDataSource<any>(aux_list);
       console.log(JSON.stringify(this.listReservas));
     },()=>{
       this.matSnackBar.open('Reserva error','Close',{
@@ -65,9 +87,8 @@ export class ReservaComponent implements OnInit {
     this.getReservasInsite();
   }
 
-
-
-
-
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  urlConf:'http://localhost:8080'
+  urlConf:'http://localhost:8080',
+  insite:250,
+  outsite:50
 };
