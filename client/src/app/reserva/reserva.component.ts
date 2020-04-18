08916711d5073bf858fd8b4e25d8cc8bb138f12b
Francesc <elskater98@gmail.com>
2020-04-18 19:44:22 +0200

    Outside and Inside works

--- client/src/app/reserva/reserva.component.ts
+++ client/src/app/reserva/reserva.component.ts
@@ -15,14 +15,18 @@ import {environment} from '../../environments/environment.prod';
 
 export class ReservaComponent implements OnInit {
   public newReservaDialogRef: MatDialogRef<ReservaCreateComponent>;
-  public listReservas:any;
+  public listReservasOutside:any;
+  public listReservasInside:any;
   public date:Date;
   public isDay:boolean;
-  public inside:boolean;
   public displayedColumns:string[]=['position','client','people','hour','observations'];
-  public dataSource: MatTableDataSource<any>;
-  public totalPeople:number;
-  public maxInsite: number;
+  public dataSourceInside: MatTableDataSource<any>;
+  public dataSourceOutside: MatTableDataSource<any>;
+  public maxInside: number;
+  public maxOutside: number;
+  public isInside:boolean;
+  private totalPeopleInside: number;
+  private totalPeopleOutside: number;
 
   constructor(
     private router: Router,
@@ -34,13 +38,16 @@ export class ReservaComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.listReservas=[];
+    this.listReservasInside=[];
+    this.listReservasOutside=[];
     this.date= new Date();
     this.isDay=true;
-    this.inside=true;
-    this.getReservasInsite();
-    this.totalPeople=0;
-    this.maxInsite= environment.insite;
+    this.getReservas();
+    this.totalPeopleInside=0;
+    this.totalPeopleOutside=0;
+    this.maxInside= environment.insite;
+    this.maxOutside= environment.outsite;
+    this.isInside=true;
   }
 
   newReserva(){
@@ -48,10 +55,10 @@ export class ReservaComponent implements OnInit {
       height: '800px',
       width: '600px',
     });
-    this.newReservaDialogRef.afterClosed().subscribe(()=> this.getReservasInsite());
+    this.newReservaDialogRef.afterClosed().subscribe(()=> this.getReservas());
   }
 
-  getReservasInsite(){
+  getReservas(){
     let aux = this.isDay ? " Lunch":" Diner";
     let subId= this.generateSubId(this.date);
 
@@ -73,14 +80,43 @@ export class ReservaComponent implements OnInit {
           'observations':data[i]['observations']
         });
       }
-      this.listReservas=aux_list;
-      this.dataSource = new MatTableDataSource<any>(aux_list);
-      console.log(JSON.stringify(this.listReservas));
+      this.listReservasInside=aux_list;
+      this.dataSourceInside = new MatTableDataSource<any>(aux_list);
+      this.countPeopleInside(aux_list);
+      //console.log(JSON.stringify(this.listReservas));
     },()=>{
       this.matSnackBar.open('Reserva error','Close',{
         duration:2000});
-    })
+    });
+
+    this.reservaService.findBySubIdAndOutside(subId+aux).subscribe((data:any) =>{
+      let aux_list=[];
+      for(let i=0; i<data.length;i++){
+        aux_list.push({
+          'position':i+1,
+          'id': data[i]['id'],
+          'client': data[i]['client'],
+          'people': data[i]['people'],
+          'subId': data[i]['subId'],
+          'date': data[i]['date'],
+          'dateString':this.datePipe.transform(data[i]['dateString'], 'dd-MM-yyyy'),
+          'hour': data[i]['hour'],
+          'inside':data[i]['inside'],
+          'mobile':data[i]['mobile'],
+          'email':data[i]['email'],
+          'observations':data[i]['observations']
+        });
+      }
+      this.listReservasOutside=aux_list;
+      this.dataSourceOutside = new MatTableDataSource<any>(aux_list);
+      this.countPeopleOutside(aux_list);
+      //console.log(JSON.stringify(this.listReservas));
+    },()=>{
+      this.matSnackBar.open('Reserva error','Close',{
+        duration:2000});
+    });
   }
+
 
   generateSubId(date:Date){
     return this.datePipe.transform(date, 'yyyy-MM-dd');
@@ -88,11 +124,32 @@ export class ReservaComponent implements OnInit {
 
   changeIsDay(){
     this.isDay = !this.isDay;
-    this.getReservasInsite();
+    this.getReservas();
   }
 
-  applyFilter(event: Event) {
+  countPeopleInside(list:any){
+    let count = 0;
+    for(let i of list){
+      count+=i['people'];
+    }
+    console.log(count);
+    this.totalPeopleInside= count;
+  }
+  countPeopleOutside(list:any){
+    let count = 0;
+    for(let i of list){
+      count+=i['people'];
+    }
+    console.log(count);
+    this.totalPeopleOutside= count;
+  }
+
+  applyFilterInside(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSourceInside.filter = filterValue.trim().toLowerCase();
+  }
+  applyFilterOutside(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceOutside.filter = filterValue.trim().toLowerCase();
   }
 }
